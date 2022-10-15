dbd5ee75f940ce5028871a522a0a7f4dcf89f593
Eren Özdemir <eren2624@gmail.com>
2022-10-15 21:56:13 +0300

    Business

---
+++ PostGIS_WebAPI.BUSINESS/Abstract/IGenericService.cs
@@ -0,0 +1,22 @@
+﻿using PostGIS_WebAPI.ENTITIES.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostGIS_WebAPI.BUSINESS.Abstract
+{
+    public interface IGenericService<T> where T : BaseEntity
+    {
+        bool Add(T item);
+        bool Add(List<T> item);
+        bool Update(T item);
+        bool Remove(T item);
+        T GetById(int id);
+        T GetDefault(Expression<Func<T, bool>> exp);
+        List<T> GetByDefault(Expression<Func<T, bool>> exp);
+        bool Activate(T item);
+    }
+}
---
+++ PostGIS_WebAPI.BUSINESS/Concrete/GenericManager.cs
@@ -0,0 +1,72 @@
+﻿using PostGIS_WebAPI.BUSINESS.Abstract;
+using PostGIS_WebAPI.ENTITIES.Entities;
+using PostGIS_WebAPI.REPOSITORIES.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostGIS_WebAPI.BUSINESS.Concrete
+{
+    public class GenericManager<T> : IGenericService<T> where T : BaseEntity
+    {
+        private readonly IGenericRepository<T> _repo;
+
+        public GenericManager(IGenericRepository<T> repo)
+        {
+            _repo = repo;
+        }
+        public bool Activate(T item)
+        {
+            if(item == null)
+                return false;
+
+            return Activate(item);
+        }
+
+        public bool Add(T item)
+        {
+            if (item == null)
+                return false;
+
+           return _repo.Add(item);
+        }
+
+        public bool Add(List<T> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetByDefault(Expression<Func<T, bool>> exp)
+        {
+            return _repo.GetByDefault(exp);
+        }
+
+        public T GetById(int id)
+        {
+            return _repo.GetById(id);
+        }
+
+        public T GetDefault(Expression<Func<T, bool>> exp)
+        {
+            return _repo.GetDefault(exp);
+        }
+
+        public bool Remove(T item)
+        {
+            if (item == null)
+                return false;
+            return _repo.Remove(item);
+        }
+
+        public bool Update(T item)
+        {
+            if (item == null)
+                return false;
+
+            return _repo.Update(item);
+        }
+    }
+}
